f8aa95c005c7bc6dd4ce8db1d219d10f7df7e809
Ruth <ruth.in5667@gmail.com>
2024-03-03 01:33:51 +0200

    update

--- TodoApi/Program.cs
+++ TodoApi/Program.cs
@@ -5,16 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<ToDoDbContext>(options =>
     options.UseMySql("name=ToDoDB", new MySqlServerVersion(new Version(8, 0, 36)))
 );
-// builder.Services.AddCors(options =>
-// {
-//     options.AddDefaultPolicy(builder =>
-//     {
-//         builder.AllowAnyOrigin()
-//                .AllowAnyMethod()
-//                .AllowAnyHeader()
-//                .AllowCredentials();
-//     });
-// });
+
  
 builder.Services.AddControllers(); // הוסף את שירותי MVC
 builder.Services.AddEndpointsApiExplorer();
